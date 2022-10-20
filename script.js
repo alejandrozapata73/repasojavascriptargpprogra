b7268b420d4549719c2ce0220c772a197e613270
alejandro <alejandrozapata73@gmail.com>
2022-10-20 10:40:43 -0300

    subiendo ultimos cambios

--- script.js
+++ script.js
@@ -28,10 +28,14 @@ var medias = 700;
 
 console.log(camiseta);
 
-var usuario1 = "Marcos Aurelio";
+const   NOMBREEMPRESA = "Ropa A&M";
+
+var usuario1 = prompt("Ingrese nombre de Usuario");
+
+
 
 var producto1 = "camiseta";
-var precioProducto1 = 2500;
+var precioProducto1 = 3500;
 var cantidadProducto1 = 15;
 var stockProducto1 = true;
 
@@ -41,13 +45,43 @@ var stockProducto2= false;
 
 console.log("El usuario: " + usuario1);
 console.log("el usuario compro: " + producto1);
+
+var cantidadProductos = parseFloat(prompt("ingrese cantidad articulos"));
+
+console.log(cantidadProductos);
+
 console.log("el usuario compro 2: " + producto1);
 console.log("Descuento $200: ")
-var totalPagar = precioProducto1 * 2;
+var totalPagar = precioProducto1 * cantidadProductos;
 var descuento = 200;
 console.log("total a pagar: " + (totalPagar - descuento))
 
 console.log("Estock: " + producto1 + " actual: " + (cantidadProducto1-2))
+
+const PI = 3.1415;
+const DOLAR = 1;
+const  IVA = 21;
+const anioNacimiento = 1990;
+const DNI = 31256987;
+
+// var usuario2 = prompt("ingrese un nombre");// un valor del tipo texto/String
+
+// var numero1 = prompt("ingrese un numero");
+// numero1 = Number(numero1);
+// var numero2 = prompt("ingrese otro numero");
+// numero2 = Number(numero2);
+
+
+// var numero1 = Number(prompt("ingrese un numero"));
+
+// var numero2 = Number(prompt("ingrese otro numero"));
+
+// var total = numero1 + numero2;
+
+// console.log("el usuario ingreso: " + usuario2);
+// console.log("el usuario numero: " + numero1);
+// console.log(numero1+numero2);
+// alert("total a pagar: " + total );
 
 
 
