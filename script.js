5756d36810b102ef9c3e8fc8613dac3c2586f848
alejandro <alejandrozapata73@gmail.com>
2022-10-20 10:02:56 -0300

    cambios en js

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
 console.log("hola probando");
-console.log("hola probando");
-console.log("hola probando");
+
+console.log(true);//verdad.....1
+console.log(false);//mentira...0
+
+console.log("marco" + "eugenia");
+
+console.log("Marcos compro: " + (1500+20000));
+
+console.log("Marcos compro: " + (20000+7000));
+
+//comentarios...
+/*
+
+multi linea
+*/
+
+//declarando variable
+var nombre1;
+
+//asignando un valor (string, numero, boolean)
+nombre1 = "Marcos Aurelio";
+
+console.log(nombre1);
+
+var camiseta = 2500;
+var medias = 700;
+
+console.log(camiseta);
+
+var usuario1 = "Marcos Aurelio";
+
+var producto1 = "camiseta";
+var precioProducto1 = 2500;
+var cantidadProducto1 = 15;
+var stockProducto1 = true;
+
+var producto2 = "mediasAdedos";
+var precioProducto2 = 700;
+var stockProducto2= false;
+
+console.log("El usuario: " + usuario1);
+console.log("el usuario compro: " + producto1);
+console.log("el usuario compro 2: " + producto1);
+console.log("Descuento $200: ")
+var totalPagar = precioProducto1 * 2;
+var descuento = 200;
+console.log("total a pagar: " + (totalPagar - descuento))
+
+console.log("Estock: " + producto1 + " actual: " + (cantidadProducto1-2))
+
+
 
 
 
